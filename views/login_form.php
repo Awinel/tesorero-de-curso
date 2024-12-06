6a5e43ee1ecea75462bbec1eb1d2a0f01a40f082
Awinel <benjamin.huerta.awi@gmail.com>
2024-12-06 17:28:49 -0300

    css link update
    Creation of the script for the css file in all the views folder

--- views/login_form.php
+++ views/login_form.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="/tesorero-de-curso/css/main.css">
 </head>
 
 <body>
--- views/logout.php
+++ views/logout.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salir</title>
+    <link rel="stylesheet" href="/tesorero-de-curso/css/main.css">
 </head>
 
 <body>
